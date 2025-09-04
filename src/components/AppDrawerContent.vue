6677d1bafdb587e63d32c16137e4cbbc76a0f176
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-09-04 21:59:50 +0300

    создала компонент для контента в дровере

---
+++ src/components/AppDrawerContent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="$style.drawerContent">
+    <AppSelectComponent />
+    <AppSelectedUser username="Иванов Иван" />
+  </div>
+</template>
+<script setup lang="ts">
+import AppSelectComponent from "@/components/ui/AppSelectComponent.vue";
+import AppSelectedUser from "@/components/ui/AppSelectedUser.vue";
+</script>
+<style module>
+.drawerContent {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-top: 20px;
+}
+</style>

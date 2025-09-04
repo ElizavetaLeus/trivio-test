2a46a87da457326611929b2a922bd2276a447f90
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-09-04 16:25:17 +0300

    добавила компонент селект

---
+++ src/components/ui/AppSelectComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <form :class="$style.selectContainer">
+    <select name="">
+      <option value="">Выберите сотрудников</option>
+    </select>
+  </form>
+</template>
+<script setup lang="ts">
+</script>
+<style module>
+.selectContainer {}
+.selectLabel {}
+</style>

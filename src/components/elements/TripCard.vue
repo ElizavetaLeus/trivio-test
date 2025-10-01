ff18137f8851f5fbc11905e49efe0515b7e4f45f
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-10-01 22:13:09 +0300

    добавила типизацию

--- src/components/elements/TripCard.vue
+++ src/components/elements/TripCard.vue
@@ -5,7 +5,7 @@
         <div :class="$style.tripId"> {{ dateToFormat(getStartTrip, 'DD.MM.YYYY') }} </div>
         <div :class="$style.tripPrice"></div>
       </div>
-      <div :class="$style.tripRoute"> </div>
+      <div :class="$style.tripRoute"> Поездка в анапу </div>
     </header>
     <AppButton
       text="Перейти к поездке ➝"
@@ -17,11 +17,101 @@
 <script setup lang="ts">
 import AppButton from '@/components/ui/AppButton.vue';
 import { getStartTrip, dateToFormat } from '@/assets/helper/date-helper';
+import { type User } from '@/utils/UtilUser';
 
+type passengers = Array<User>;
+interface Ticket {
+  id: number,
+  provider: string,
+  placeFrom: string,
+  placeTo: string,
+  dateFrom: string,
+  dateTo: string,
+  timeFrom: string,
+  timerTo: string,
+  iataFrom: string,
+  iataTo: string,
+  price: number,
+}
+interface Service {
+  user: User,
+  ticket: Ticket,
+}
+interface Trip {
+  id: number,
+  name: string,
+  price: number,
+  passengers: User[],
+  services: Service[],
+  status: string,
+}
+
+const trip: Trip =
+  {
+    id: 2,
+    name: 'Поездка в Москву',
+    price: 10600,
+    passengers: [
+      {
+        id: 3,
+        first_name: 'Попов',
+        second_name: 'Михаил',
+        last_name: 'Андреевич',
+      },
+    ],
+    services: [
+      {
+        user: {
+          id: 3,
+          first_name: 'Попов',
+          second_name: 'Михаил',
+          last_name: 'Андреевич',
+        },
+        ticket: {
+          id: 3,
+          provider: 'Pobeda',
+          placeFrom: 'Казань',
+          placeTo: 'Москва',
+          dateFrom: '27-08-2025',
+          dateTo: '27-08-2025',
+          timeFrom: '07:12',
+          timerTo: '09:05',
+          iataFrom: 'KZN',
+          iataTo: 'DME',
+          price: 5100,
+        },
+      },
+      {
+        user: {
+          id: 3,
+          first_name: 'Попов',
+          second_name: 'Михаил',
+          last_name: 'Андреевич',
+        },
+        ticket: {
+          id: 4,
+          provider: 'Pobeda',
+          placeFrom: 'Казань',
+          placeTo: 'Москва',
+          dateFrom: '29-08-2025',
+          dateTo: '29-08-2025',
+          timeFrom: '07:15',
+          timerTo: '09:13',
+          iataFrom: 'KZN',
+          iataTo: 'DME',
+          price: 5500,
+        },
+      },
+    ],
+    status: 'ended',
+  };
 </script>
 
 <style module>
 .root {
+  background-color: var(--color-white);
+  border-radius: 10px;
+  padding: 20px 30px;
   display: flex;
   flex-direction: column;
 }

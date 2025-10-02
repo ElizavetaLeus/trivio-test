dceda45ce3e4fa846d46106c2fa5a586b841ddfc
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-10-02 15:07:08 +0300

    добавила отрисовку поездки и всю логику по ней

--- src/components/elements/TripCard.vue
+++ src/components/elements/TripCard.vue
@@ -1,16 +1,27 @@
 <template>
   <div :class="$style.root">
-    <header :class="$style.header">
+    <header :class="[$style.header, $style.container]">
       <div :class="$style.tripInformation">
         <div :class="$style.tripId"> {{ getIDTrip(trip) }} </div>
-        <div :class="$style.tripPrice">{{ getCountPrice(trip) }}</div>
+        <div :class="$style.tripPrice">{{ formatPrice(getCountPrice(trip)) }}</div>
       </div>
       <div :class="$style.tripRoute"> {{ trip.name }} </div>
     </header>
-    <AppButton
-      text="Перейти к поездке ➝"
-      type="text"
-    />
+    <div :class="[$style.container, $style.tripList]">
+      <AppTrip
+        v-for="service in trip.services"
+        :key="service.ticket.id"
+        :passenger="service.user"
+        :route="getRoute(service.ticket)"
+        :price="formatPrice(service.ticket.price)"
+      />
+    </div>
+    <footer :class="[$style.footer, $style.container]">
+      <AppButton
+        text="Перейти к поездке ➝"
+        type="text"
+      />
+    </footer>
   </div>
 </template>
 
@@ -18,6 +29,7 @@
 import AppButton from '@/components/ui/AppButton.vue';
 import { getStartTrip, dateToFormat } from '@/assets/helper/date-helper';
 import { type User } from '@/utils/UtilUser';
+import AppTrip from '@/components/ui/AppTrip.vue';
 
 type passengers = Array<User>;
 interface Ticket {
@@ -105,7 +117,6 @@ const trip: Trip =
     ],
     status: 'ended',
   };
-
 const getIDTrip = (trip: Trip) => {
   return `#${trip.id} от ${dateToFormat(getStartTrip(), 'DD.MM.YYYY')}`;
 };
@@ -114,15 +125,24 @@ const getCountPrice = (trip: Trip) => {
   const price = allServices.map((service: Service) => {
     return service.ticket.price;
   }).reduce((acc, curr) => acc + curr, 0);
+
+  return price;
+};
+const formatPrice = (price: number) => {
   return new Intl.NumberFormat('ru-RU').format(price) + ' ₽';
+};
+const getRoute = (ticket: Ticket) => {
+  return `${ticket.placeFrom} ➝ ${ticket.placeTo} ${dateToFormat(getStartTrip(), 'DD.MM')}`;
 };
 </script>
 
 <style module>
+.container {
+  padding: 20px 30px;
+}
 .root {
   background-color: var(--color-white);
   border-radius: 10px;
-  padding: 20px 30px;
   display: flex;
   flex-direction: column;
 }
@@ -130,6 +150,8 @@ const getCountPrice = (trip: Trip) => {
   border-bottom: 1px solid var(--color-black-10);
   display: flex;
   flex-direction: column;
+  gap: 10px;
+  padding-bottom: 20px;
 }
 .tripInformation {
   display: flex;
@@ -144,5 +166,21 @@ const getCountPrice = (trip: Trip) => {
 }
 .tripPrice {
   font-weight: 700;
+}
+.tripRoute {
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 1;
+}
+.tripList {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.footer {
+  border-top: 1px solid var(--color-black-10);
+  padding-top: 20px;
+  display: flex;
+  justify-content: flex-end;
 }
 </style>

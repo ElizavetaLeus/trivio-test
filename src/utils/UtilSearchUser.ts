0b08cd1fe65fdcacb34b87c9827c84f3d532e4d8
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-10-16 22:29:38 +0300

    удалила лишний файл

--- src/utils/UtilSearchUser.ts
+++
@@ -1,8 +0,0 @@
-import { type User } from '@/utils/UtilUser';
-
-const searchUser = (id: string | number, userList: User[]) => {
-  const user = userList.find(user => user.id === id);
-  console.log(id);
-  return user;
-};
-export default searchUser;

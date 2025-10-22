4a9bafaa9ae6a1d524aeeb6ca4baf1a9ac5b6e8c
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-10-23 00:10:47 +0300

    добавила метод createTrip

--- src/api/trips/index.ts
+++ src/api/trips/index.ts
@@ -1,7 +1,9 @@
 import HTTP from '@/helper/http';
 import { type Trip } from '@/types/Trip';
+import { useRouter } from 'vue-router';
 
 const http = new HTTP();
+const router = useRouter();
 
 export const tripsApi = {
   async getTrips() {
@@ -15,6 +17,16 @@ export const tripsApi = {
     const response = await http.post<Trip, Body>('/trips', body);
     if (response.data) {
       return response.data;
+    }
+    return null;
+  },
+  async getTripById(id: string | string[]) {
+    const response = await http.get<Trip>(`/trips/${id}`);
+    if (response.data) {
+      return response.data;
+    }
+    if (response.error || response.data === undefined) {
+      router.push({ name: 'home' });
     }
     return null;
   },

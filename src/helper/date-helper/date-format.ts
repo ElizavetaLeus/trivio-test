ee365c1a0a10b8aa56263c436daa3adab2e92727
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-10-24 21:28:11 +0300

    выделила форматирование даты в отдельную функцию

---
+++ src/helper/date-helper/date-format.ts
@@ -0,0 +1,10 @@
+import { dateReverse } from '@/helper/date-helper/index';
+
+const dateFormat = (date: string, time: string) => {
+  const dateTime = `${dateReverse(date)} ${time}`;
+  return {
+    dateTime: new Date(dateTime).toISOString(),
+    timeStamp: new Date(dateTime).getTime(),
+  };
+};
+export default dateFormat;

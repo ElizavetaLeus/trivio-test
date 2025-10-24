8e330d910957fa9859b104e7528939b8ec174b60
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-10-24 21:28:57 +0300

    экспортировала date-format

--- src/helper/date-helper/index.ts
+++ src/helper/date-helper/index.ts
@@ -1,5 +1,11 @@
 import dateReverse from '@/helper/date-helper/date-reverse';
 import dateToFormat from '@/helper/date-helper/date-to-format';
 import getStartTrip from '@/helper/date-helper/get-start-trip';
+import dateFormat from '@/helper/date-helper/date-format';
 
-export { dateReverse, dateToFormat, getStartTrip };
+export {
+  dateReverse,
+  dateToFormat,
+  getStartTrip,
+  dateFormat,
+};

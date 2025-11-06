cc3d7ef4bddf299f445546a792c81b784e4f630d
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-11-06 21:51:04 +0300

    отрисовала селект в странице поиска

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,3 +1,18 @@
 <template>
-  <div>SEARCH</div>
+  <div :class="$style.root">
+    <UserSelect :maxWidth="250"/>
+  </div>
 </template>
+<script setup lang="ts">
+
+import UserSelect from '@/components/elements/UserSelect.vue';
+</script>
+<style module>
+.root {
+  margin-top: 50px;
+  display: flex;
+  gap: 21px;
+  align-items: center;
+}
+
+</style>

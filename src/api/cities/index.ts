c16f5cfd92967e36d9b86132509c79bf9eeb1a6e
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-11-12 23:37:54 +0300

    добавила апи для получения городов

---
+++ src/api/cities/index.ts
@@ -0,0 +1,13 @@
+import HTTP from '@/helper/http';
+
+const http = new HTTP();
+
+export const cityApi = {
+  async getUsers() {
+    const response = await http.get<string[]>('/cities');
+    if(response.data) {
+      return response.data;
+    }
+    return [];
+  },
+};

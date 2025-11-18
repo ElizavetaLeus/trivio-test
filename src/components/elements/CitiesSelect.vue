ff8f5ddb10ce40972bbd0d68480324de0e9eeac2
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-11-18 17:55:59 +0300

    поменяла стили для селекта

--- src/components/elements/CitiesSelect.vue
+++ src/components/elements/CitiesSelect.vue
@@ -1,6 +1,8 @@
 <template>
   <div :class="$style.selectWrapper">
-    <AppIcon name="airplane" />
+    <div :class="$style.iconWrapper">
+      <AppIcon name="airplane" />
+    </div>
     <select :class="$style.citiesSelect">
       <option>{{ text }}</option>
       <option
@@ -25,13 +27,20 @@ defineProps<Props>();
 
 <style module>
 .selectWrapper {
-  background-color: var(--color-gray-light);
   color: var(--color-primary);
+  position: relative;
+}
+.iconWrapper {
   display: flex;
-  gap: 10px;
+  align-items: center;
   justify-content: center;
-  padding: 10px;
-  border-radius: 5px;
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+  pointer-events: none;
 }
 .citiesSelect {
   color: var(--color-gray);
@@ -39,6 +48,9 @@ defineProps<Props>();
   outline: none;
   border: none;
   width: 100%;
+  padding: 10px 10px 10px 40px;
+  border-radius: 5px;
+  height: 40px;
 }
 .citiesSelect:focus {
   border-color: transparent;

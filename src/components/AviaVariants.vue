e27ac366ec8f157085aebf64b453176e68cc79b2
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-11-19 17:31:23 +0300

    создала файл, сделала фейковый чекбокс

---
+++ src/components/AviaVariants.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="$style.root">
+    <h1 :class="$style.title">
+      Найдено
+      <span :class="$style.sumVariants">349</span>
+      Вариантов
+    </h1>
+    <div :class="$style.filterVariant">
+      <label>
+        <input
+          type="checkbox"
+          :class="$style.originCheckbox"
+          :checked="isChecked"
+          @change="handleChange()"
+        />
+        <span :class="$style.fakeCheckbox"></span>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  isChecked: boolean;
+}
+interface Emits {
+  (event: 'change'): void;
+}
+defineProps<Props>();
+const emits = defineEmits<Emits>();
+const handleChange = (): void => {
+  emits('change');
+};
+</script>
+
+<style module>
+.originCheckbox {
+  display: none;
+}
+.originCheckbox:checked + .fakeCheckbox::before {
+  background-color: var(--color-primary);
+}
+.fakeCheckbox {
+  --size: 10px;
+  width: var(--size);
+  height: var(--size);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  background-color: var(--color-white);
+  border: 1px solid var(--color-primary);
+  transition: all 0.3s ease;
+}
+.fakeCheckbox::before {
+  content: '';
+  --size: 6px;
+  width: var(--size);
+  height: var(--size);
+  display: flex;
+  background-color: transparent;
+  border-radius: 50%;
+}
+</style>

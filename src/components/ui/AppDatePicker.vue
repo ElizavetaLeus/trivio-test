06347d0fc5b65c0928941d7e96dbd44dce9622d3
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-11-24 16:56:30 +0300

    переделала календарь, добавила иконку по макету

--- src/components/ui/AppDatePicker.vue
+++ src/components/ui/AppDatePicker.vue
@@ -1,19 +1,38 @@
 <template>
-  <input
-    type="date"
-    :id="id"
-    :class="$style.calendar"
-  />
+  <div :class="$style.root">
+    <div :class="$style.iconWrapper">
+      <AppIcon name="airplane" />
+    </div>
+    <input
+      type="date"
+      :class="$style.calendar"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
-interface Props {
-  id: string,
-}
-defineProps<Props>();
+import AppIcon from '@/components/ui/AppIcon.vue';
+
 </script>
 
 <style module>
+.root{
+  position: relative;
+}
+.iconWrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+  pointer-events: none;
+  z-index: 1;
+  color: var(--color-primary);
+}
 .calendar {
   height: 40px;
   background-color: var(--color-gray-light);
@@ -23,5 +42,14 @@ defineProps<Props>();
   border: none;
   padding: 10px;
   cursor: pointer;
+}
+.calendar[type="date"] {
+  position: relative;
+  padding-inline-start: 40px;
+}
+.calendar[type="date"]::-webkit-calendar-picker-indicator {
+  position: absolute;
+  left: 10px;
+  opacity: 0;
 }
 </style>

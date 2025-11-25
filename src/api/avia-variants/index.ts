57aaef14215e99bcd69ad4fe4f868e57f50b4da0
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-11-26 00:50:16 +0300

    переименовала

--- src/api/avia-variants/index.ts
+++ src/api/avia-variants/index.ts
@@ -2,8 +2,8 @@ import HTTP from '@/helper/http';
 import { type Ticket } from '@/types/Ticket';
 
 const http = new HTTP();
-export const TicketApi = {
-  async getTicket() {
+export const AviaVariantApi = {
+  async getAviaVariants() {
     const response = await http.get<Ticket[]>('/aviaVariants');
     if (response.data) {
       return response.data;

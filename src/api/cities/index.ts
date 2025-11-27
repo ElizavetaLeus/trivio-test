e82c4c4e110f7d9c0a8bd22e094ba8895293ef46
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-11-27 18:59:04 +0300

    поправила api в связи с тем, что переделала бэк

--- src/api/cities/index.ts
+++ src/api/cities/index.ts
@@ -1,12 +1,13 @@
 import HTTP from '@/helper/http';
+import { type City } from '@/types/City';
 
 const http = new HTTP();
 
 export const cityApi = {
   async getCities() {
-    const response = await http.get<string[]>('/cities');
+    const response = await http.get<City[]>('/cities');
     if(response.data) {
-      return response.data;
+      return response.data.map(city => city.name);
     }
     return [];
   },

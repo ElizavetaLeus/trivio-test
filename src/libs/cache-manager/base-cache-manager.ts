088a73fe2783919614bc37aaf3876ca057ff1bfd
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-12-09 13:46:17 +0300

    добавила таймлимит

--- src/libs/cache-manager/base-cache-manager.ts
+++ src/libs/cache-manager/base-cache-manager.ts
@@ -1,15 +1,36 @@
 export class BaseCacheManager {
   protected manager = localStorage;
 
+  protected ONE_SECOND = 1000; // mile second
+  protected ONE_HOUR = this.ONE_SECOND * 60 * 60;
+  protected ONE_DAY = this.ONE_HOUR * 24;
+  protected DAY_30 = this.ONE_DAY * 30;
+  protected SAVE_DEFAULT = this.ONE_HOUR;
+
   protected keys = {
-    cities: 'CITIES',
+    cities: {
+      name: 'CITIES',
+      save: this.ONE_DAY,
+    },
   };
 
-  protected set<Value>(key: keyof typeof this.keys, value: Value) {
-    this.manager.setItem(key, value);
+  protected set<Value>(keyName: keyof typeof this.keys, value: Value) {
+    const timeLimit = new Date().getTime() + this.keys[keyName].save;
+    this.manager.setItem(keyName, JSON.stringify({
+      ended: timeLimit,
+      value: value,
+    }));
   }
   protected get(key: keyof typeof this.keys) {
-    this.manager.getItem(key);
+    const itemBase = this.manager.getItem(keyName);
+    if (itemBase !== null) {
+      const item = JSON.parse(itemBase);
+      if (item.ended > new Date().getTime()) {
+        return item.value;
+      } else {
+        this.remove(key);
+      }
+    }
   }
   protected remove(key: keyof typeof this.keys) {
     this.manager.removeItem(key);
--- src/libs/cache-manager/cache-manager.ts
+++ src/libs/cache-manager/cache-manager.ts
@@ -2,9 +2,9 @@ import { BaseCacheManager } from '@/libs/cache-manager/base-cache-manager';
 
 export class CacheManager extends BaseCacheManager {
   setCities<Value> (value: Value){
-    this.set(this.keys.cities, value);
+    this.set(this.keys.cities.name, value);
   }
   getCities() {
-    this.get(this.keys.cities);
+    this.get(this.keys.cities.name);
   }
 }

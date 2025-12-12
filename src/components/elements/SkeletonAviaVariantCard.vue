9b87af71b06be2414efebaab78b3a1af215a1080
ElizavetaLeus <elizaveta_lavrenova@inbox.ru>
2025-12-12 21:19:14 +0300

    переделала скелетон

--- src/components/elements/SkeletonAviaVariantCard.vue
+++ src/components/elements/SkeletonAviaVariantCard.vue
@@ -1,15 +1,15 @@
 <template>
   <div :class="$style.root">
-    <div :class="$style.carrier">
-      <div :class="[$style.skeletonShimmer, $style.carrierText]"></div>
+    <div>
+      <div :class="['skeleton-block', $style.carrierText]"></div>
     </div>
     <div :class="$style.tripContent">
-      <div :class="[$style.dateTime, $style.skeletonShimmer]"></div>
-      <div :class="[$style.roadMap, $style.skeletonShimmer]"></div>
-      <div :class="[$style.dateTime, $style.skeletonShimmer]"></div>
+      <div :class="['skeleton-block', $style.dateTime]"></div>
+      <div :class="['skeleton-block', $style.roadMap]"></div>
+      <div :class="['skeleton-block', $style.dateTime]"></div>
     </div>
     <footer :class="$style.footer">
-      <div :class="[$style.skeletonShimmer, $style.button]"></div>
+      <div :class="['skeleton-block', $style.button]"></div>
     </footer>
   </div>
 </template>
@@ -21,64 +21,35 @@
   padding: 20px;
   display: flex;
   flex-direction: column;
-  width: 100%;
-}
-.carrier {
-  margin-bottom: 15px;
 }
 .tripContent {
   display: flex;
+  justify-content: space-between;
   gap: 40px;
   padding-bottom: 20px;
-  padding-top: 15px;
-  justify-content: space-between;
-  border-bottom: 1px solid var(--color-black-10);
+  margin-top: 30px;
 }
 .dateTime {
-  width: 70px;
-  height: 43px;
+  --width: 70px;
+  --height: 43px;
 }
 .roadMap {
-  max-width: 580px;
-  width: 100%;
+  --width: 580px;
+  --height: 43px;
 }
 .footer {
   padding-top: 20px;
   display: flex;
   justify-content: flex-end;
-}
-.skeletonShimmer {
-  background: var(--color-white-smoke);
-  border-radius: 4px;
-  position: relative;
-  overflow: hidden;
-}
-.skeletonShimmer::after {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: -100%;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(90deg, transparent, var(--color-gray-bright), transparent);
-  animation: shimmer 1.2s infinite;
+  border-top: 1px solid var(--color-black-10);
 }
 .carrierText {
-  height: 16px;
-  width: 120px;
+  --height: 16px;
+  --width: 120px;
 }
 .button {
-  height: 44px;
-  width: 239px;
-  border-radius: 8px;
-}
-
-@keyframes shimmer {
-  0% {
-    left: -100%;
-  }
-  100% {
-    left: 100%;
-  }
+  --height: 44px;
+  --width: 239px;
+  --border-radius: 8px;
 }
 </style>
